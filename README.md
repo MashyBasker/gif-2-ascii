0890f7d06b03602ff4d6836e34f92f5ad95bd679
Maharshi Basu <basumaharshi10@gmail.com>
2024-02-22 01:03:34 +0530

    add all codes and README.md
    Signed-off-by: Maharshi Basu <basumaharshi10@gmail.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# gif2ascii
+
+A python script that converts a GIF to ASCII
+
+## Quickstart
+
+```console
+$ python -m venv .venv # create virtual environment
+$ source .venv/bin/activate # activate the environment
+$ pip install -r requirements.txt
+# add the path to GIF file inside the code
+$ python gif2ascii.py
+```
+
+## Demo
+
+
+### Spinning Cube
+<img src="./media/spinning-cube.gif" width=400 height=300>
+
+### Weird Expanding Cubes
+<img src="./media/expanding_cube.gif" width=400 height=300>
+
---
+++ gif2ascii.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import math
+import os
+from time import sleep
+
+
+#text = "#$@&MH%?;:=^~-"[::-1]
+text = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+def convert_gif_to_frames(gif):
+    num = 0
+    frames = []
+    _, frame = gif.read()
+    while True:
+        try:
+            ok, frame = gif.read()
+            if not ok:
+                print("FUCKKK")
+                break
+            frame = cv2.resize(frame, (100,50))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frames.append(frame)
+            num += 1
+        except KeyboardInterrupt:
+            break
+    gif.release()
+    return frames
+
+
+def frame_to_ascii(frame, n):
+    out = []
+    img = np.array(frame)
+    h, w = img.shape
+    for i in range(h):
+        row = []
+        for j in range(w):
+            row.append(text[math.floor((img[i][j]/255)*n -1)])
+        out.append(row)
+    os.system("clear")
+    for i in range(h):
+        for j in range(w):
+            print(out[i][j], end="")
+        print()
+
+def main():
+    n = len(text)
+    gif = cv2.VideoCapture('./cubes.gif')
+    if not gif.isOpened():
+        print("Err: Can't open")
+    frame_list = convert_gif_to_frames(gif)
+    d = len(frame_list)
+    idx = 0
+    while True:
+        frame_to_ascii(frame_list[idx], n)
+        idx = (idx+1)%d
+        sleep(0.03)
+
+if __name__ == '__main__':
+    main()
+
